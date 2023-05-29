061ccf20b31ee6deb081709fb27b2a445150f61c
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-29 11:43:25 -0300

    Servicio de la cuenta

---
+++ mockito-service/src/main/java/com/sistemasactivos/mockitoservice/servicios/ServicioCuenta.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.mockitoservice.servicios;
+
+import com.sistemasactivos.mockitoservice.modelos.Cuenta;
+
+import java.math.BigDecimal;
+
+public interface ServicioCuenta {
+    Cuenta findById(Long id);
+    int revisarTotalTransferencias(Long bancoId);
+    BigDecimal revisarSaldo(Long cuentaId);
+    void transferir(Long numeroCuentaOrigen, Long numeroCuentaDestino, BigDecimal monto);
+
+}
