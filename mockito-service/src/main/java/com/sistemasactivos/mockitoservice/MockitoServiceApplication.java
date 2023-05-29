cb89d56d8b7ae93b0f5059fc4bfc2e47ba60cc9f
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-29 11:21:48 -0300

    Inicio del proyecto

---
+++ mockito-service/src/main/java/com/sistemasactivos/mockitoservice/MockitoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.mockitoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoServiceApplication.class, args);
+    }
+
+}
