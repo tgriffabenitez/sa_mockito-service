70fc85de5d6fccce28e37e55b1ab639a65da738a
tgriffabenitez <tgriffabenitez@gmail.com>
2023-05-29 11:59:07 -0300

    Clase datos donde implemento todos los atributos estaticos

---
+++ mockito-service/src/main/java/com/sistemasactivos/mockitoservice/Datos.java
@@ -0,0 +1,12 @@
+package com.sistemasactivos.mockitoservice;
+
+import com.sistemasactivos.mockitoservice.modelos.Banco;
+import com.sistemasactivos.mockitoservice.modelos.Cuenta;
+
+import java.math.BigDecimal;
+
+public class Datos {
+    public static final Cuenta CUENTA_001 = new Cuenta(1L, "Andres", new BigDecimal("1000"));
+    public static final Cuenta CUENTA_002 = new Cuenta(2L, "John", new BigDecimal("2000"));
+    public static final Banco BANCO = new Banco(1L, "El banco financiero", 0);
+}
